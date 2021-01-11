e232c5d840d782851b9ce2c84b2e1d50fe0f8811
zhujingxing <zhujingxing@hd123.com>
2021-01-11 16:40:54 +0800

    一些输出测试

--- src/main/java/com/zjx/learn/site/controller/SiteController.java
+++ src/main/java/com/zjx/learn/site/controller/SiteController.java
@@ -2,7 +2,6 @@ package com.zjx.learn.site.controller;
 
 import com.zjx.learn.log.LogTest;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/main/java/com/zjx/learn/test/SopTest.java
@@ -0,0 +1,24 @@
+package com.zjx.learn.test;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @program: learn
+ * @description main方法，一些输出测试
+ * @author: zhujingxing
+ * @create: 2021-01-11 16:24
+ **/
+public class SopTest {
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format = simpleDateFormat.format(new Date());
+        System.out.println(format);
+        System.out.println(new Date());
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        String s = timestamp.toString();
+        System.out.println(s);
+        System.out.println(new Timestamp(new Date().getTime()));
+    }
+}
